66f4d658ee9dde7dcce729760cb100cec7692af5
gajgele <gajicdr@gmail.com>
2016-12-12 15:08:37 +0100

    Tipovi podataka

---
+++ Gaja02 - Tipovi podataka/src/App.java
@@ -0,0 +1,35 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/**
+		 * 
+		 * 
+		 */
+		
+		int myNumber = 88;
+		short myShort = 847;
+		long myLong = 9797;
+		double myDouble = 7.654;
+		float myFloat = 324.3f;
+		char myChar = 'y';
+		boolean myBoolean = false;
+		byte myByte = 127;
+		
+		/*
+		 * 
+		 */
+				
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+	}
+
+}
